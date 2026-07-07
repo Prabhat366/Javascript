75ed276d7431769792286941c76bee300dfa0af6
Prabhat Jena <prabhatjena785@gmail.com>
2026-07-07 07:54:46 +0530

    Add files via upload

---
+++ 06_string.js
@@ -0,0 +1,56 @@
+// const name = "Prabhat";
+// const study = "JavaScript";
+
+// console.log(name+study);
+
+//console.log(`Hello my name is ${name} and I'm studying ${study}`);//String Interpolation
+
+// String Initialization
+
+const MyName = new String ("Prabhat")
+
+// console.log(MyName.length);
+// console.log(MyName.toUpperCase());
+// console.log(MyName.charAt(1));
+// console.log(MyName.indexOf('r'));
+
+// const newstrign=MyName.substring(0,6)
+// console.log(newstrign);
+
+const anotherString = MyName.slice(-7,7)
+// console.log(anotherString);//Prabhat
+
+const NewString = "   prabhat   "
+// console.log(NewString);
+// console.log(NewString.trim());
+
+const url = "hhtps://google.com%20/facebook.com"
+// console.log(url.replace('%20', "_"));
+// console.log(url.includes('instagram'));
+
+//all the methods of string 
+/*
+String length
+String charAt()
+String charCodeAt()
+String codePointAt()
+String concat()
+String at()
+String [ ]
+String slice()
+String substring()
+String substr()
+String toUpperCase()
+String toLowerCase()
+String isWellFormed()
+String toWellFormed()
+String trim()
+String trimStart()
+String trimEnd()
+String padStart()
+String padEnd()
+String repeat()
+String replace()
+String replaceAll()
+String split()
+*/
